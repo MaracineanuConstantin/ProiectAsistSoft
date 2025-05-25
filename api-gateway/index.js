5c83bc8b6b3eead32310f91d3e09113df743c649
MaracineanuConstantin <95632363+MaracineanuConstantin@users.noreply.github.com>
2025-05-25 20:15:39 +0300

    Update index.js

--- api-gateway/index.js
+++ api-gateway/index.js
@@ -5,7 +5,7 @@ import jwt from 'jsonwebtoken';
 
 const app = express();
 app.use(cors({
-    origin: 'http://localhost:3000', // Your Svelte app's port
+    origin: 'http://localhost:3000',
     credentials: true
 }));
 app.use(express.json());
